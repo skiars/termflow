3722507ee9eb702612298daa8c5d0a87f0a5ad05
Skiars <skiars@qq.com>
2026-01-19 16:46:38 +0800

    Refactor MonadFlow to use MonadUnliftIO and update withRunFlow implementation

--- src/Termflow/Core.hs
+++ src/Termflow/Core.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Termflow.Core
   ( FlowT (..),
+    RunFlowIO,
+    MonadUnliftIO,
     runFlowT,
     withRunFlow,
   )
@@ -21,6 +23,7 @@ import Control.Monad.IO.Class
 import Control.Monad.Reader
 import Control.Monad.State.Class (MonadState)
 import Control.Monad.Writer.Class (MonadWriter)
+import Control.Monad.IO.Unlift (MonadUnliftIO, withRunInIO)
 import Termflow.Class
 import Termflow.Types
 
@@ -38,22 +41,23 @@ newtype FlowT m a = FlowT {unFlowT :: ReaderT (TQueue FlowEvent) m a}
       MonadTrans,
       MonadState s,
       MonadWriter w,
-      MonadError e
+      MonadError e,
+      MonadUnliftIO
     )
 
 instance (MonadReader r m) => MonadReader r (FlowT m) where
   ask = lift ask
   local f (FlowT m) = FlowT (mapReaderT (local f) m)
 
+type RunFlowIO m b = (forall a. m a -> IO a) -> IO b
+
 -- | Run the FlowT monad
 runFlowT :: TQueue FlowEvent -> FlowT m a -> m a
 runFlowT q (FlowT m) = runReaderT m q
 
 -- | Capture the current flow runner to execute flow actions within the underlying monad.
-withRunFlow :: (Monad m) => ((forall a. FlowT m a -> m a) -> m b) -> FlowT m b
-withRunFlow inner = do
-  q <- FlowT ask
-  lift $ inner (runFlowT q)
+withRunFlow :: (MonadUnliftIO m) => RunFlowIO m b -> m b
+withRunFlow = withRunInIO
 
 -- | Helper to emit an event
 emit :: (MonadIO m) => FlowEvent -> FlowT m ()
